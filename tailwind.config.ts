30c18a57128b8b28fd2158d60ae333a36718cacb
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-18 14:26:35 -0400

    🎨 refactor: lint code and ignore certain folders

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,37 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [require("daisyui")],
-}
-export default config
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#ffd500",
+          secondary: "#fdc500",
+          accent: "#FFD60A",
+          neutral: "#001D3D",
+          "base-100": "#000814",
+          success: "#a4f371",
+          warning: "#e1e233",
+          error: "#ffcaca",
+          info: "#524000",
+        },
+      },
+    ],
+  },
+};
+export default config;
