cd980bddfaf56f3f317e3eee0c9d07b5ec12e3f3
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-26 21:50:06 -0400

    ✨ feat(cart): Add functionality to update item quantity in cart actions
    Imports the updateCart function from @/utils.
    
    Adds a new updateItemQuantity function to update the quantity of an item in the cart.
    
    Handles cases where the cart ID is missing or the quantity is set to 0.
    
    Updates the cart using the updateCart function and revalidates the cart tag.
    
    Includes error handling for potential issues during the update process.

--- src/components/ui/cart/actions.ts
+++ src/components/ui/cart/actions.ts
@@ -1,5 +1,5 @@
 "use server";
-import { cartCreate, getCart, addToCart } from "@/utils";
+import { cartCreate, getCart, addToCart, updateCart } from "@/utils";
 import { revalidatePath, revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 import { removeProductFromCart } from "@/utils/index";
@@ -41,5 +41,41 @@ export async function removeItem(prevState: any, lineId: string) {
     revalidateTag("cart");
   } catch (e) {
     return "Error removing item from cart";
+  }
+}
+
+export async function updateItemQuantity(
+  prevState: any,
+  payload: {
+    lineId: string;
+    variantId: string;
+    quantity: number;
+  },
+) {
+  const cartId = cookies().get("cartId")?.value;
+
+  if (!cartId) {
+    return "Missing cart ID";
+  }
+
+  const { lineId, variantId, quantity } = payload;
+
+  try {
+    if (quantity === 0) {
+      await removeProductFromCart(cartId, [lineId]);
+      revalidateTag("cart");
+      return;
+    }
+
+    await updateCart(cartId, [
+      {
+        id: lineId,
+        merchandiseId: variantId,
+        quantity,
+      },
+    ]);
+    revalidateTag("cart");
+  } catch (e) {
+    return "Error updating item quantity";
   }
 }
