b4b4e6b4aad8ba37757a420b9c31df28f97d60fa
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-30 20:36:24 -0400

    ✨ feat(navbar): Add search functionality and enhance layout
    Import:
    Adds an import for the SearchBar component.
    Navbar structure:
    Updates the overall structure:
    Changes the class for the navbar container to sticky top-0 z-20 bg-base-100 flex justify-around gap-2.
    Wraps the logo in a div with the class hidden md:block to hide it on smaller screens.
    Includes the SearchBar component.
    Rearranges the Cart and User components within a flex gap-2 container.

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import User from "./user";
 import Cart from "../ui/cart";
+import SearchBar from "../ui/search/search-bar";
 
 function Navbar() {
   return (
-    <div className="navbar bg-base-100/40">
-      <div className="flex-1">
-        <a href="/" className="btn btn-ghost text-xl">
-          mik store
+    <div className="navbar sticky top-0  z-20  bg-base-100 flex justify-around gap-2">
+      <div className="hidden md:block">
+        <a href="/" className="font-bold text-xl">
+          MK
         </a>
       </div>
-      <div className="flex-none gap-4">
+      <SearchBar />
+      <div className="flex gap-2">
         <Cart />
-        <div className="dropdown dropdown-end">
-          <div tabIndex={0} role="button" className=" ">
-            <User />
-          </div>
+
+        <div tabIndex={0} role="button" className="h-8 aspect-square">
+          <User />
         </div>
       </div>
     </div>
