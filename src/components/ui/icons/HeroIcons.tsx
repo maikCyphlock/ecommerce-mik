b5de70c409b1bb28e6bf4ca59ee10c72d2b702d8
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-30 21:24:38 -0400

    💄 feat(heroicons): Add HeroIcons

---
+++ src/components/ui/icons/HeroIcons.tsx
@@ -0,0 +1,71 @@
+export const PlusCircle = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0a9 9 0 0 1 18 0"
+    ></path>
+  </svg>
+);
+
+export const MinusCircle = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M15 12H9m12 0a9 9 0 1 1-18 0a9 9 0 0 1 18 0"
+    ></path>
+  </svg>
+);
+
+export const XMark20Solid = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 20 20"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94z"
+    ></path>
+  </svg>
+);
+
+export const MagnifyingGlass = (props) => (
+  <svg
+    width="1em"
+    height="1em"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      fill="none"
+      stroke="#000000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="m21 21l-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607"
+    ></path>
+  </svg>
+);
