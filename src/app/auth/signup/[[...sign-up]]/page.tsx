5cfabe948f315cc0c151a5352e56b49586de5522
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-30 20:31:44 -0400

    💄 style(auth): Apply styling to login and signup pages

--- src/app/auth/signup/[[...sign-up]]/page.tsx
+++ src/app/auth/signup/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className="grid min-h-screen place-content-center">
+      <SignUp />
+    </div>
+  );
 }
