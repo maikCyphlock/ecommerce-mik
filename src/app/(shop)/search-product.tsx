c4083869a73e667528f31a3772b20d3156987c95
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-30 21:23:11 -0400

    ✨ feat(shop): Introduce product search functionality for effortless discovery
    - Create SearchProduct component to fetch and display results
    -  Connect search parameters to the component for dynamic queries
    - Integrate ProductCard to showcase individual products
    - Construct search page layout for seamless user experience

---
+++ src/app/(shop)/search-product.tsx
@@ -0,0 +1,24 @@
+import { getProductFromSearch } from "@/utils";
+import React, { useEffect, useState } from "react";
+import ProductCard from "@/components/product/product-card";
+async function SearchProduct({ query }) {
+  if (!query) return null;
+  // create a debounce function
+
+  const { data } = await getProductFromSearch(query);
+
+  return (
+    <div className="grid justify-items-center grid-flow-dense grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 mt-12 mb-32 p-2">
+      {data?.search?.edges?.map(({ node }) => (
+        <ProductCard
+          key={node?.id}
+          id={node?.id}
+          name={node?.title}
+          imgUrl={node?.images?.nodes?.[0]?.url}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default SearchProduct;
---
+++ src/app/(shop)/search/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/layout/footer";
+
+import SearchProduct from "../search-product";
+export default function Home({ searchParams }) {
+  return (
+    <>
+      <SearchProduct query={searchParams?.query} />
+
+      <Footer />
+    </>
+  );
+}
