3f7c0bf9e7dbf47e3a7cf616d9bc9d46bc90c1bc
maikCyphlock <maikolaguilar656@gmail.com>
2023-12-30 20:27:13 -0400

    ♻️ refactor(home): Restructure Home component layout and add search functionality

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "@/components/layout/footer";
 import ShowProduct from "@/components/product/show-product";
-
-export default function Home() {
+import SearchProduct from "./search-product";
+import { CtaMain } from "@/components/shacdn/cta-main";
+export default function Home({ searchParams }) {
   return (
     <>
-      <ShowProduct />
+      <CtaMain />
+      <ShowProduct id="collections" />
       <Footer />
     </>
   );
