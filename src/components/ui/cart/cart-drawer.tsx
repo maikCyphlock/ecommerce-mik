e18a3cbc61cdbc6f0543a283648334b398e35bfc
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-01 20:28:43 -0400

    ♻️ feat(cart-drawer): Refactor cart drawer for smoother interactions with Radix UI and Shadcn
    Integrate Sheet component for polished dialog experience
    
    Enhance styling for visual consistency
    
    Migrate content to SheetContent for better organization
    
    Position cart summary for optimal readability

--- src/components/ui/cart/cart-drawer.tsx
+++ src/components/ui/cart/cart-drawer.tsx
@@ -1,5 +1,6 @@
 import { FormatCurrency } from "@/utils";
-
+import { Sheet, SheetTrigger, SheetContent } from "../sheet";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import DeleteItemCart from "./delete-item-cart";
 import EditItemQuantityButton from "./edit-cart-btn";
 
@@ -23,124 +24,113 @@ export const ShoppingBag = (props) => (
 );
 export const CartDrawer = ({ cartProducts }) => {
   return (
-    <div className="drawer z-10 ">
-      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+    <Sheet>
       <div className="drawer-content ">
-        <button className=" drawer-button" aria-label="open sidebar">
-          <label
-            htmlFor="my-drawer"
-            className="cursor-pointer flex justify-center items-center"
-            aria-label="cart"
-          >
-            <ShoppingBag className="h-6 w-6" />
-            {cartProducts?.totalQuantity && (
-              <span
-                aria-label="cart items"
-                className="badge badge-sm indicator"
-              >
-                {cartProducts?.totalQuantity}
-              </span>
-            )}
-          </label>
-        </button>
-      </div>
-      <div className="drawer-side">
-        <ul
-          role="list"
-          aria-label="sidebar with items of cart"
-          className="menu p-4 w-80 min-h-full bg-base-200 text-base-content justify-between"
-        >
-          <label
-            className="btn bt-ghost btn-circle btn-sm absolute top-2 right-2"
-            aria-label="close sidebar"
-            htmlFor="my-drawer"
-          >
-            X
-          </label>
-          <div className="flex flex-col gap-4 overscroll-contain">
-            {cartProducts?.lines?.edges?.map(({ node }) => (
-              <div
-                className="relative flex gap-2 mt-5"
-                data-gid={node?.merchandise?.id}
-                key={node?.merchandise?.id}
-              >
-                <DeleteItemCart merchandiseId={node?.id} />
-
-                <img
-                  src={node?.merchandise?.image?.url}
-                  className="w-24 aspect-square object-cover rounded-md"
-                  alt=""
-                />
-                <section className="flex flex-col gap-4">
-                  <div className="flex flex-col gap-1">
-                    <h2 className="text-lg font-bold text-base-content">
-                      {node.merchandise?.product?.title}
-                    </h2>
-                    <p>{node?.merchandise?.title}</p>
-                    <span>
-                      {FormatCurrency(
-                        node.estimatedCost?.totalAmount?.amount,
-                        node?.estimatedCost?.totalAmount?.currencyCode,
-                      )}
-                    </span>
-                  </div>
-                  <span className="border-secondary rounded-badge border flex gap-3 justify-center items-center">
-                    <EditItemQuantityButton item={node} type="minus" />
-                    <span className="text-lg font-bold text-secondary">
-                      {node?.quantity}
-                    </span>
-                    <EditItemQuantityButton item={node} type="plus" />
-                  </span>
-                </section>
-              </div>
-            ))}
-          </div>
-          <section className="flex flex-col self-center  mt-10">
-            <div className="flex justify-between items-center">
-              <h2 className=" text-lg">Subtotal</h2>
-              <p className="flex gap-2 font-bold">
-                <span>
-                  {FormatCurrency(
-                    cartProducts?.estimatedCost?.subtotalAmount?.amount,
-                    cartProducts?.estimatedCost?.subtotalAmount?.currencyCode,
-                  )}
-                </span>
-              </p>
-            </div>
-            <div className="divider"></div>
-            <div className="flex justify-between items-center">
-              <h2 className=" text-lg">Taxes</h2>
-              <p className="flex gap-2 font-bold">
-                <span>
-                  {FormatCurrency(
-                    cartProducts?.estimatedCost?.totalTaxAmount?.amount,
-                    cartProducts?.estimatedCost?.totalTaxAmount?.currencyCode,
-                  )}
-                </span>
-              </p>
-            </div>
-            <div className="divider"></div>
-            <div className="flex justify-between items-center">
-              <h2 className=" text-lg">Total</h2>
-              <p className="flex gap-2 font-bold">
-                <span>
-                  {FormatCurrency(
-                    cartProducts?.estimatedCost?.totalAmount?.amount,
-                    cartProducts?.estimatedCost?.totalAmount?.currencyCode,
-                  )}
-                </span>
-              </p>
-            </div>
-            <div className="divider"></div>
-            <a
-              href={cartProducts?.checkoutUrl}
-              className="btn btn-primary btn-wide "
+        <SheetTrigger asChild>
+          <button className=" drawer-button" aria-label="open sidebar">
+            <label
+              htmlFor="my-drawer"
+              className="cursor-pointer flex justify-center items-center"
+              aria-label="cart"
             >
-              Checkout
-            </a>
-          </section>
-        </ul>
+              <ShoppingBag className="h-6 w-6" />
+              {cartProducts?.totalQuantity && (
+                <span
+                  aria-label="cart items"
+                  className="badge badge-sm indicator"
+                >
+                  {cartProducts?.totalQuantity}
+                </span>
+              )}
+            </label>
+          </button>
+        </SheetTrigger>
       </div>
-    </div>
+      <SheetContent className="overflow-y-auto bg-base-100 dark:bg-base-100">
+        <div className="flex flex-col gap-4 ">
+          {cartProducts?.lines?.edges?.map(({ node }) => (
+            <div
+              className="relative flex gap-2 mt-5"
+              data-gid={node?.merchandise?.id}
+              key={node?.merchandise?.id}
+            >
+              <DeleteItemCart merchandiseId={node?.id} />
+
+              <img
+                src={node?.merchandise?.image?.url}
+                className="w-24 aspect-square object-cover rounded-md"
+                alt=""
+              />
+              <section className="flex flex-col gap-4">
+                <div className="flex flex-col gap-1">
+                  <h2 className="text-lg font-bold text-base-content">
+                    {node.merchandise?.product?.title}
+                  </h2>
+                  <p>{node?.merchandise?.title}</p>
+                  <span>
+                    {FormatCurrency(
+                      node.estimatedCost?.totalAmount?.amount,
+                      node?.estimatedCost?.totalAmount?.currencyCode,
+                    )}
+                  </span>
+                </div>
+                <span className="border-secondary rounded-badge border flex gap-3 justify-center items-center">
+                  <EditItemQuantityButton item={node} type="minus" />
+                  <span className="text-lg font-bold text-secondary">
+                    {node?.quantity}
+                  </span>
+                  <EditItemQuantityButton item={node} type="plus" />
+                </span>
+              </section>
+            </div>
+          ))}
+        </div>
+
+        <section className="flex flex-col self-center  mt-10">
+          <div className="flex justify-between items-center">
+            <h2 className=" text-lg">Subtotal</h2>
+            <p className="flex gap-2 font-bold">
+              <span>
+                {FormatCurrency(
+                  cartProducts?.estimatedCost?.subtotalAmount?.amount,
+                  cartProducts?.estimatedCost?.subtotalAmount?.currencyCode,
+                )}
+              </span>
+            </p>
+          </div>
+          <div className="divider"></div>
+          <div className="flex justify-between items-center">
+            <h2 className=" text-lg">Taxes</h2>
+            <p className="flex gap-2 font-bold">
+              <span>
+                {FormatCurrency(
+                  cartProducts?.estimatedCost?.totalTaxAmount?.amount,
+                  cartProducts?.estimatedCost?.totalTaxAmount?.currencyCode,
+                )}
+              </span>
+            </p>
+          </div>
+          <div className="divider"></div>
+          <div className="flex justify-between items-center">
+            <h2 className=" text-lg">Total</h2>
+            <p className="flex gap-2 font-bold">
+              <span>
+                {FormatCurrency(
+                  cartProducts?.estimatedCost?.totalAmount?.amount,
+                  cartProducts?.estimatedCost?.totalAmount?.currencyCode,
+                )}
+              </span>
+            </p>
+          </div>
+          <div className="divider"></div>
+          <a
+            href={cartProducts?.checkoutUrl}
+            className="btn btn-primary btn-block "
+          >
+            Checkout
+          </a>
+        </section>
+      </SheetContent>
+    </Sheet>
   );
 };
