9af5aad262d50674fc54792c88eb3574b5940699
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-01 20:25:44 -0400

    ✨ fix(product-gallery): Enhance image navigation with a smooth scrollbar
    Integrate ScrollArea and ScrollBar components for effortless viewing
    
    Adapt styling to gracefully handle horizontal overflow
    
    Conditionally display the scrollbar on larger screens for optimized space usage

--- src/components/product/product-gallery.tsx
+++ src/components/product/product-gallery.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -7,26 +8,29 @@ export function ProductGallery({ product }) {
 
   return (
     <>
-      <div className="flex lg:flex-col gap-2 ">
-        {product?.images?.nodes?.map((node) => (
-          <Image
-            key={node?.id}
-            alt={product?.title}
-            src={node?.src}
-            width={node?.width || 800}
-            height={node?.height || 800}
-            loading="lazy"
-            id={node?.src}
-            onClick={() =>
-              setActiveImage(product?.images?.nodes?.indexOf(node))
-            }
-            className={`h-32 w-32 aspect-square object-cover object-center rounded border hover:border-secondary transition-colors duration-200 cursor-pointer ${
-              activeImage === product?.images?.nodes?.indexOf(node) &&
-              "border-neutral"
-            }`}
-          />
-        ))}
-      </div>
+      <ScrollArea>
+        <div className="flex lg:flex-col gap-2 ">
+          {product?.images?.nodes?.map((node) => (
+            <Image
+              key={node?.id}
+              alt={product?.title}
+              src={node?.src}
+              width={node?.width || 800}
+              height={node?.height || 800}
+              loading="lazy"
+              id={node?.src}
+              onClick={() =>
+                setActiveImage(product?.images?.nodes?.indexOf(node))
+              }
+              className={`h-32 w-32 aspect-square object-cover object-center rounded border hover:border-secondary transition-colors duration-200 cursor-pointer ${
+                activeImage === product?.images?.nodes?.indexOf(node) &&
+                "border-neutral"
+              }`}
+            />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" className="hidden lg:block" />
+      </ScrollArea>
       <div>
         <Image
           alt={product?.title}
