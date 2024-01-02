775021e538dc16ad95a4a6e99b0a520464632ef7
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-02 13:23:47 -0400

    ♻️ refactor: Enhance linting tools and component organization for maintainability ️
    Expand linting for broader code quality checks (eslint addition)
    
    Relocate ShoppingBag icon to a dedicated icons file for better structure
    
    Remove unnecessary parseFloat calls for optimized currency formatting
    
    Streamline code for readability and maintainability

--- src/components/ui/cart/cart-drawer.tsx
+++ src/components/ui/cart/cart-drawer.tsx
@@ -3,25 +3,7 @@ import { Sheet, SheetTrigger, SheetContent } from "../sheet";
 import DeleteItemCart from "./delete-item-cart";
 import EditItemQuantityButton from "./edit-cart-btn";
 import { CartProducts } from "@/interfaces/cart";
-
-export const ShoppingBag = (props) => (
-  <svg
-    width="1em"
-    height="1em"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-    {...props}
-  >
-    <path
-      fill="none"
-      stroke="#000000"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="1.5"
-      d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007M8.625 10.5a.375.375 0 1 1-.75 0a.375.375 0 0 1 .75 0m7.5 0a.375.375 0 1 1-.75 0a.375.375 0 0 1 .75 0"
-    ></path>
-  </svg>
-);
+import { ShoppingBag } from "../icons/hero-icons";
 export const CartDrawer = ({
   cartProducts,
 }: {
@@ -132,7 +114,7 @@ export const CartDrawer = ({
           <a
             href={cartProducts?.checkoutUrl}
             className="btn btn-primary btn-block "
-            disabled={cartProducts?.lines.edges.length ? false : true}
+            disabled={!cartProducts?.lines.edges.length}
           >
             Checkout
           </a>
