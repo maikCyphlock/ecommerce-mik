064e908a9dc3d442220f84ab63429adec606d42a
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-02 18:45:35 -0400

    ♻️ refactor: Enhance search with informative feedback for a more intuitive experience
    Gracefully handle empty search results with a clear "no products found" message
    
    Optimize data extraction for efficiency by pre-mapping nodes
    
    Maintain visual consistency by ensuring alignment with grid layout even when no results are found

--- src/app/(shop)/search-product.tsx
+++ src/app/(shop)/search-product.tsx
@@ -6,17 +6,19 @@ async function SearchProduct({ query }) {
   // create a debounce function
 
   const { data } = await getProductFromSearch(query);
-
+  const nodes = data?.search?.edges?.map((node) => node);
   return (
     <div className="grid justify-items-center grid-flow-dense grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 mt-12 mb-32 p-2">
-      {data?.search?.edges?.map(({ node }) => (
-        <ProductCard
-          key={node?.id}
-          id={node?.id}
-          name={node?.title}
-          imgUrl={node?.images?.nodes?.[0]?.url}
-        />
-      ))}
+      {nodes.length > 0
+        ? nodes.map(({ node }) => (
+            <ProductCard
+              key={node?.id}
+              id={node?.id}
+              name={node?.title}
+              imgUrl={node?.images?.nodes?.[0]?.url}
+            />
+          ))
+        : "no products found"}
     </div>
   );
 }
