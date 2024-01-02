db1c0e6d016f6ed5bf27ec518375ab474aa05119
maikCyphlock <maikolaguilar656@gmail.com>
2024-01-02 18:43:37 -0400

    ✨ feat(navbar): Unfold a responsive navbar experience with a dropdown menu for mobile users
    Introduce a dropdown menu to gracefully house cart, login, and subscription options on smaller screens
     Employ the visually appealing EllipsisVertical20Solid icon as the dropdown trigger
    
    Rearrange navbar elements for optimal responsiveness across different viewports
    
    Relocate SearchBar import for enhanced component organization

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,24 +1,53 @@
 import React from "react";
 import User from "./user";
 import Cart from "../ui/cart";
-import SearchBar from "../ui/search/search-bar";
+import SearchBar from "@/components/ui/search/search-bar";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuItem,
+} from "@/components/ui/dropdown-menu";
+import { EllipsisVertical20Solid } from "../ui/icons/hero-icons";
 
 function Navbar() {
   return (
-    <div className="navbar sticky top-0  z-20  bg-base-100 flex justify-around gap-2">
+    <div className="navbar sticky top-0  z-20  bg-base-100 flex md:justify-around justify-center gap-2">
       <div className="hidden md:block">
         <a href="/" className="font-bold text-xl">
           MK
         </a>
       </div>
       <SearchBar />
-      <div className="flex gap-2 items-stretch">
+      <div className="hidden md:flex gap-2 items-stretch">
         <Cart />
 
         <div tabIndex={0} role="button" className="h-8 aspect-square">
           <User />
         </div>
       </div>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger className="md:hidden">
+          <EllipsisVertical20Solid className="w-6 h-6" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className=" md:hidden bg-base-100 dark:bg-base-100 border-base-200 text-base-content dark:text-base-content">
+          <DropdownMenuLabel>
+            {" "}
+            <Cart />
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem></DropdownMenuItem>
+          <DropdownMenuItem className="gap-1">
+            <User />
+            Login
+          </DropdownMenuItem>
+
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
